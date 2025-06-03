7d1c8d0cac969bfc55ede5312960e3f2be765098
Arpita Mallik <arpitamallik13@gmail.com>
2025-06-03 13:55:06 +0600

    test file added

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def predict():
     return render_template('home.html', prediction_text='The house price prediction is {}'.format(output))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
